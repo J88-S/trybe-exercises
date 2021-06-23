11877e81b0cee59bfcb00f2f6886b5aba3f7fdcc
Josie <seuemail@exemplo.br>
2021-06-23 18:37:27 -0300

    Exercicio 2 conteúdo 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -6,3 +6,12 @@ console.log('Subtração: ' + (a - b));
 console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
+
+let numero= 12;
+let numero1= 22;
+
+if (numero1 > numero){
+    console.log("É o maior número!");
+} else {
+    console.log("0");
+}
