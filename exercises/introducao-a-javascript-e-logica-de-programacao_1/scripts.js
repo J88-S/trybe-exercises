23046859380a891ae9dd575107d798e5522cac65
Josie <seuemail@exemplo.br>
2021-06-23 19:07:06 -0300

    Exercicio 6 conteúdo 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -49,3 +49,29 @@ if (todosAngulos){
 } else {
     console.log("Erro:inválido");
 }
+
+let xadrez = 'rainha';
+
+switch (xadrez.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+};
