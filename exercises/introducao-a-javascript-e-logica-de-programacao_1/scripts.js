020f079e4e2856daab70ecc3f6c14e57934d0253
Josie <seuemail@exemplo.br>
2021-06-23 19:11:51 -0300

    Exercicio 7 conteúdo 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -75,3 +75,22 @@ switch (xadrez.toLowerCase()) {
     console.log('Erro, peça inválida!');
     break;
 };
+
+
+let nota = 66;
+
+if (nota < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
