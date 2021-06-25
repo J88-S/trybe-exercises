197d2dffb593f7efee765b42869b7f2b41458a24
Josie <seuemail@exemplo.br>
2021-06-25 17:09:38 -0300

    Adiciona jogada diagonal da rainha

--- exercises/fundamentos/scripts.js
+++ exercises/fundamentos/scripts.js
@@ -1,12 +1,31 @@
-let frutas = [3, 4, 10, 1, 12];
-let soma = 0;
+//qual a posiçao da rainha?
+let posicaoRainhaLinha = 4;
+let posicaoRainhaColuna = 4;
+// qual a posiçao da peça?
+let posicaoPecaLinha = 2;
+let posicaoPecaColuna = 2;
+// Algo para guardar se o ataque foi bem sucedido ou nao
+let ataqueBemSucedido = false;
 
-for (let index = 0; index < frutas.length; index +=1) {
-    soma += frutas[index];
+//quais sao os caminhos que a rainha pode atacar
+
+// se a peca estiver na mesma linha que a rainha, atque bem sucedido
+if (posicaoPecaLinha === posicaoPecaLinha) {
+    console.log("Ataque bem sucedido na mesma linha");
+    ataqueBemSucedido = true;
 }
 
-if (soma > 15) {
-    console.log(soma);
-} else {
-    console.log("Valor menor que 16");
+if (posicaoPecaColuna === posicaoRainhaColuna) {
+    console.log("Ataque bem sucedido na mesma linha");
+    ataqueBemSucedido = true;
+}
+for (let index = 1; index< 8; index += 1) {
+    let linhaAtualRainha = posicaoRainhaLinha - index;
+    let colunaAtualRainha = posicaoRainhaColuna - index;
+    //Se a linha atual da rainha for igual a posicao da peca linha e a coluna atual da rainha for igual a posicao da peca coluna, mostre ataque bem sucedido.
+    if (posicaoPecaLinha === linhaAtualRainha && posicaoPecaColuna === colunaAtualRainha) {
+        console.log("Ataque bem sucedido na diagonal inferior esquerdo");
+        ataqueBemSucedido = true;
+        break;
+    }
 }
