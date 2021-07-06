8af68a489692ffc8cfa1262fe1de4de46a3370ef
Josie <seuemail@exemplo.br>
2021-07-06 19:21:21 -0300

    Exercício 8

--- exercises/dia-3-javascript-eventos/scripts.js
+++ exercises/dia-3-javascript-eventos/scripts.js
@@ -132,3 +132,15 @@ function dayMouseOver() {
   };
   
   newTaskSpan('Projeto:');
+
+  function newTaskDiv(color) {
+
+    let tasksContainer = document.querySelector('.my-tasks');
+    let newTask = document.createElement('div');
+  
+    newTask.className = 'task';
+    newTask.style.backgroundColor = color;
+    tasksContainer.appendChild(newTask);
+  };
+  
+  newTaskDiv('green');
