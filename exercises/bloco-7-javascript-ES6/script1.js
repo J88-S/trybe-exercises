bb2c70f662afd67dda140d1e6bd430fe63ca87b6
Josie <seuemail@exemplo.br>
2021-07-27 14:24:58 -0300

    Testando ainda

--- exercises/bloco-7-javascript-ES6/script1.js
+++ exercises/bloco-7-javascript-ES6/script1.js
@@ -11,7 +11,25 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
-const expected = sum(4, "5");
+const expected = sum(4, "5"); //(4, 5), (0,0)
 
 assert.ok(expected === 9);
 assert.strictEqual(expected, "nove");
+
+//Exercício 2
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+const expected = myRemove([1, 2, 3, 4], 3);
+
